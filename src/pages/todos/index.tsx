12a8f92fee7dd410ad4eb379b41900f9ae5db39f
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-19 13:41:29 -0300

    feat: add todos page

---
+++ src/pages/todos/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../../components/Header'
+import { Container } from './styles'
+
+export default function Todos() {
+  return (
+    <>
+      <Container> Hello World </Container>
+    </>
+  )
+}
---
+++ src/pages/todos/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.bg};
+  min-height: 100vh;
+  color: white;
+`
