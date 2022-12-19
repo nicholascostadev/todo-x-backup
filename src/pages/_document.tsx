30d3d400ec2b4bf59e967e7d479a72d4474bf9d9
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-19 16:23:54 -0300

    refactor: change _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,6 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
-import stylisRTLPlugin from 'stylis-plugin-rtl'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(
-              <StyleSheetManager stylisPlugins={[stylisRTLPlugin]}>
-                <App {...props} />
-              </StyleSheetManager>,
-            ),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
   render() {
     return (
       <Html>
