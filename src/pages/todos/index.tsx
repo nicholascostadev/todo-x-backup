dd45a9826d6f9cd0110e6f80be21a15e1db43499
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-19 14:22:18 -0300

    Add Todo component to page

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,11 +1,15 @@
 import { Header } from '../../components/Header'
+import { Todos } from '../../components/Todos'
 import { Container } from './styles'
 
-export default function Todos() {
+export default function TodosPage() {
   return (
     <>
       <Header />
-      <Container> Hello World </Container>
+      <Container>
+        <Todos />
+        <div></div>
+      </Container>
     </>
   )
 }
