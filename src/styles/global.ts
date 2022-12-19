347f4d406df07afd70da08776cd2bc93edc23e4c
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-19 11:53:05 -0300

    fix syntax error for margin and padding

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,8 +3,8 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   * {
-    margin: 0,
-    padding: 0,
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
     font-family: "Roboto", sans-serif;
   }
