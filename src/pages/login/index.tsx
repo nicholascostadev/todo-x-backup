69cd70a23101cb49366139e441a0fab3a5089099
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-20 21:07:51 -0300

    feat: Add basic login validation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,35 @@
 import Image from 'next/image'
 import { GoogleLogo } from 'phosphor-react'
-import { FormEvent } from 'react'
-import { Container, Form, FormContainer, ImageContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import {
+  Container,
+  Form,
+  FormContainer,
+  FormError,
+  FormInput,
+  ImageContainer,
+} from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const loginSchema = z.object({
+  email: z.string().min(1, 'Email é obrigatório').email('Email inválido'),
+  password: z.string().min(1, 'Senha é obrigatória'),
+})
+
+type Login = z.infer<typeof loginSchema>
 
 export default function LoginPage() {
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<Login>({
+    resolver: zodResolver(loginSchema),
+  })
+  const handleFormSubmit = (data: Login) => {
+    // no server currently
+    return data
   }
   return (
     <Container>
@@ -17,22 +41,26 @@ export default function LoginPage() {
         />
       </ImageContainer>
       <FormContainer>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit(handleFormSubmit)}>
           <h2>Login</h2>
-          <label htmlFor="email" >Email</label>
-          <input
+          <label htmlFor="email">Email</label>
+          <FormInput
             type="text"
             placeholder="email@exemplo.com"
-            name="email"
             id="email"
+            error={!!errors.email}
+            {...register('email')}
           />
+          <FormError>{errors.email?.message}</FormError>
           <label htmlFor="password">Senha</label>
-          <input
+          <FormInput
             type="text"
             placeholder="Senha"
-            name="password"
             id="password"
+            error={!!errors.password}
+            {...register('password')}
           />
+          <FormError>{errors.password?.message}</FormError>
           <button type="submit">Entrar</button>
           <div>
             <div />
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -45,19 +45,7 @@ export const Form = styled.form`
   }
 
   label {
-    font-size: ${({ theme }) => theme.fontSize.md}; }
-
-  > input {
-    border: 1px solid ${({ theme }) => theme.colors.slate700};
-    background-color: ${({ theme }) => theme.colors.bg};
-    border-radius: ${({ theme }) => theme.rounded.sm};
     font-size: ${({ theme }) => theme.fontSize.md};
-    padding: 0.5rem 1rem;
-    color: ${({ theme }) => theme.colors.gray200};
-
-    &::placeholder {
-      color: ${({ theme }) => theme.colors.gray400};
-    }
   }
 
   > div {
@@ -130,4 +118,30 @@ export const ImageContainer = styled.div`
   position: relative;
   width: 500px;
   height: 500px;
+`
+
+type FormInputProps = {
+  error: boolean
+}
+
+export const FormInput = styled.input<FormInputProps>`
+  border: 1px solid
+    ${({ theme, error }) =>
+      error ? theme.colors.red500 : theme.colors.slate700};
+  outline: 1px solid
+    ${({ theme, error }) => (error ? theme.colors.red500 : 'auto')};
+  background-color: ${({ theme }) => theme.colors.bg};
+  border-radius: ${({ theme }) => theme.rounded.sm};
+  font-size: ${({ theme }) => theme.fontSize.md};
+  padding: 0.5rem 1rem;
+  color: ${({ theme, error }) =>
+    error ? theme.colors.red500 : theme.colors.gray200};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray400};
+  }
+`
+
+export const FormError = styled.span`
+  color: ${({ theme }) => theme.colors.red500};
 `
